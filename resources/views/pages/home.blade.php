c24de3ae644e9f3d3c8d2faf02a7576ec47116ce
Shokhrukh <shohruhhon@gmail.com>
2024-12-23 16:51:43 +0500

    fix 204

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -301,7 +301,7 @@
                 <!-- Carousel Item 3 -->
                 <div class="product-box">
                     <div class="product-img">
-                        <a href="/catalog/petlitel/204">
+                        <a href="/catalog/lapki/204">
                             <img src="/assets/images/product/j257лапка257321-5.6.jpg" alt="">
                         </a>
                         <div class="product-badge-left">
@@ -313,7 +313,7 @@
                     </div>
                     <div class="product-title">
                         <h6 class="fw-medium">
-                            <a href="/catalog/plastina/204">Лапка 257321-5.6</a>
+                            <a href="/catalog/lapki/204">Лапка 257321-5.6</a>
                         </h6>
                         <div class="add-to-wishlist">
                             <a href="#"><i class="far fa-heart"></i></a>
